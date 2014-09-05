d5113da7ece7847d6efe0de6cd2237014c51b47e
Antoine de Lachèze-Murel <a.delachezemurel@novediagroup.com>
2014-09-05 15:36:19 +0200

    [ADLM] le reste

--- src/App/AdminBundle/Listener/DoctrineExtensionListener.php
+++ src/App/AdminBundle/Listener/DoctrineExtensionListener.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: a.delachezemurel
- * Date: 05/09/14
- * Time: 11:24
- */ 
+
+
+namespace App\AdminBundle\Listener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class DoctrineExtensionListener implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function onLateKernelRequest(GetResponseEvent $event)
+    {
+        $translatable = $this->container->get('gedmo.listener.translatable');
+        $translatable->setTranslatableLocale($event->getRequest()->getLocale());
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $securityContext = $this->container->get('security.context', ContainerInterface::NULL_ON_INVALID_REFERENCE);
+        if (null !== $securityContext && null !== $securityContext->getToken() && $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $loggable = $this->container->get('gedmo.listener.loggable');
+            $loggable->setUsername($securityContext->getToken()->getUsername());
+        }
+    }
+}
