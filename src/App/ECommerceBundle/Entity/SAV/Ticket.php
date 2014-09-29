b5ca7761212b3644a7ee37a44b7541a09d5d1749
Antoine de Lachèze-Murel <a.delachezemurel@novediagroup.com>
2014-09-29 13:52:52 +0200

    [ADLM] outil de ticketinh

---
+++ src/App/ECommerceBundle/Entity/SAV/Ticket.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\ECommerceBundle\Entity\SAV;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ticket
+ *
+ * @ORM\Table(name="sav_ticket")
+ * @ORM\Entity
+ */
+class Ticket
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="state", type="string", length=255)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\OneToMany(targetEntity="\App\ECommerceBundle\Entity\Customer", mappedBy="ticket")
+     */
+    private $customer;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set state
+     *
+     * @param string $state
+     * @return Ticket
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return string 
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Ticket
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Ticket
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $customer
+     */
+    public function setCustomer($customer)
+    {
+        $this->customer = $customer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+
+}
---
+++ src/App/ECommerceBundle/Form/SAV/TicketType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\ECommerceBundle\Form\SAV;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TicketType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('type', 'choice', array(
+                'label' => 'Objet',
+                'choices' => array('Service client' => 'Service Client',
+                    'Webmaster' => 'Webmaster'),
+                'expanded' => false,
+                'multiple' => false,
+                'required' => true
+            ))
+
+
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\ECommerceBundle\Entity\SAV\Ticket'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_ecommercebundle_sav_ticket';
+    }
+}
