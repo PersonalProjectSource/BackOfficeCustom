8dabf45ee9b4107d7ba65c7645837322052b6c7a
Optimus Prime <lbrau@hotmail.com>
2014-10-14 15:18:22 +0200

    re

--- src/App/CMSBundle/Entity/Repositories/ArticleRepository.php
+++ src/App/CMSBundle/Entity/Repositories/ArticleRepository.php
@@ -1,14 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: optimus
- * Date: 14/10/2014
- * Time: 14:45
- */
+
+
 
 namespace App\CMSBundle\Entity\Repositories;
 
+use Doctrine\ORM\EntityRepository;
 
-class ArticleRepository {
+class ArticleRepository extends EntityRepository {
+
+
+    public function getArticleList($oRequest) {
+
+        //$qb = $em->getRepository('AppCMSBundle:Article')->createQueryBuilder('a');
+        //$qb->select('a');
+
+        $qb = $this->createQueryBuilder('a')->select();
+
+
+        /*$qb_count = clone $qb;
+        $qb->setFirstResult($oRequest->get('iDisplayStart'));
+        $qb->setMaxResults($oRequest->get('iDisplayLength'));
+        */
+
+
+        $result =  $qb->getQuery()->getResult();
+        var_dump($result);die;
+        //$qb_count->select('COUNT(a)');
+        //$total =  $qb_count->getQuery()->getSingleScalarResult();
+
+        return $result;
+    }
+
 
 } 
